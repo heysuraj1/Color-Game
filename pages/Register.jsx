da626323bc7049e41641243981b7ac5e3c2c6365
Suraj Singh <kumarniteshoffice786@gmail.com>
2022-11-25 00:54:12 +0530

    login & register added

--- pages/Register.jsx
+++ pages/Register.jsx
@@ -3,10 +3,12 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import Modal from "react-bootstrap/Modal";
 
-const Login = () => {
+const Register = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [show, setShow] = useState(false);
+  const [name, setName] = useState("");
+  const [contactNumber, setContactNumber] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   const router = useRouter();
@@ -21,9 +23,11 @@ const Login = () => {
 
     try {
       axios
-        .post("/api/Authentication/login", {
-          email: email,
-          password: password,
+        .post("/api/Authentication/signup", {
+          Email: email,
+          Password: password,
+          Name: name,
+          ContactNumber: contactNumber,
         })
         .then((acc) => {
           console.log(acc.data);
@@ -46,6 +50,15 @@ const Login = () => {
     <div className="container mt-5">
       <form onSubmit={handleSubmit}>
         <div className="form-group">
+          <label htmlFor="exampleInputEmail1">Name</label>
+          <input
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+            type="text"
+            className="form-control"
+            placeholder="Name"
+          />
           <label htmlFor="exampleInputEmail1">Email address</label>
           <input
             onChange={(e) => {
@@ -54,6 +67,15 @@ const Login = () => {
             type="email"
             className="form-control"
             placeholder="Enter email"
+          />
+          <label htmlFor="exampleInputEmail1">Contact Number</label>
+          <input
+            onChange={(e) => {
+              setContactNumber(e.target.value);
+            }}
+            type="text"
+            className="form-control"
+            placeholder="Contact Number"
           />
           <label htmlFor="exampleInputEmail1" className="mt-2">
             Enter Password
@@ -84,20 +106,20 @@ const Login = () => {
               style={{ width: "20%" }}
               className="btn btn-primary"
             >
-              Login
+              Register
             </button>
           </div>
         )}
       </form>
 
       <h6
-        onClick={() => router.push("/Register")}
+        onClick={() => router.push("/Login")}
         style={{ textAlign: "right", marginTop: 10, cursor: "pointer" }}
       >
-        Create Account
+        Already Have An Account
       </h6>
     </div>
   );
 };
 
-export default Login;
+export default Register;
